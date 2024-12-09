68cb8295f701738e0e63d9d9e896914440abe0cd
archilst <rendydharmaputra424@gmail.com>
2024-12-09 14:52:12 +0700

    change hero content text content & design, change env variable name

--- app/components/hero/content_hero.tsx
+++ app/components/hero/content_hero.tsx
@@ -2,12 +2,14 @@ import { CtaHero } from "./cta_hero";
 
 export const ContentHero = () => {
   return (
-    <div className="relative z-10 flex flex-col items-center lg:items-start gap-8 w-full lg:w-[664px] ">
-      <h3 className="font-bold text-center lg:text-left text-4xl md:text-5xl lg:text-[64px] text-background">
-        Harmoni Alam dalam Gaya
+    <div className="relative z-10 flex flex-col items-center lg:items-start gap-8 w-full lg:w-[800px] ">
+      <h3 className="font-bold text-center lg:text-left text-4xl md:text-5xl lg:text-[64px] lg:leading-tight text-background">
+        Transformasi Busana dengan Sentuhan Alam
       </h3>
-      <p className="font-medium text-center lg:text-left text-xl md:text-2xl text-background">
-        Busana berbahan alami untuk menciptakan dampak positif pada lingkungan
+      <p className="font-normal text-center lg:text-left text-xl md:text-2xl text-background">
+        Hadirkan keindahan dan keberlanjutan melalui ecoprint. Temukan produk
+        dan pelatihan yang memadukan seni, keunikan, dan tanggung jawab
+        lingkungan
       </p>
       <CtaHero />
     </div>
